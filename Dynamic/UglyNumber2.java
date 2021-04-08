3899e91b4c17231ca4c9886b1c494835537749f8
interviewprepdev <81889574+interviewprepdev@users.noreply.github.com>
2021-04-07 17:34:45 -0700

    Update UglyNumber2.java

--- Dynamic/UglyNumber2.java
+++ Dynamic/UglyNumber2.java
@@ -3,6 +3,7 @@ class Solution {
       // save all uglynumbers in array
       // always try to find the next min number that is a factor of 2, 3, 5
       // reason for min of 2, 3, 5 here is ugly number should only have 2, 3, 5 as factors
+      // more explnation and ref here: https://leetcode.com/problems/ugly-number-ii/discuss/69364/My-16ms-C%2B%2B-DP-solution-with-short-explanation
          if (n <= 1) return n;
          int ptr2=0, ptr3=0, ptr5=0;
          int[] ugly = new int[n];
