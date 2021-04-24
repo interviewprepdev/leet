97648d5486b69b2fb65095893121b878040382c5
interviewprepdev <81889574+interviewprepdev@users.noreply.github.com>
2021-04-24 15:13:05 -0700

    Create MaximumBinaryTree2.java

---
+++ tree/MaximumBinaryTree2.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode insertIntoMaxTree(TreeNode root, int val) {        
+        TreeNode n = new TreeNode(val);
+        if (root == null || root.val < val) {
+            n.left = root;
+            return n;
+        }
+        TreeNode cur = root;
+        while (cur.right != null && cur.right.val > val)
+        {
+            cur = cur.right;
+        }
+        
+        n.left = cur.right;
+        cur.right = n;
+        return root;
+    }
+}
